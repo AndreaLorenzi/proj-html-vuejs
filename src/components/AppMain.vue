4b176f8ad89a29190ec62a2eda1a2749b4694921
AndreaLorenzi <124718024+AndreaLorenzi@users.noreply.github.com>
2023-05-30 14:38:19 +0200

    add section 3

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -68,7 +68,59 @@
     <!-- sezione2 -->
     <section></section>
     <!-- sezione3 -->
-    <section></section>
+    <section>
+      <h3 class="title-section-3">What critics are saying about us</h3>
+
+      <div class="reviews">
+        <div class="big">
+          <div class="left-box">
+            <!-- <img src="../assets/images/blog72x.jpg" alt="" /> -->
+          </div>
+          <div class="box-sinistra">
+            <div class="reviews-text">
+              <h3>MEGGY STEWART</h3>
+              <h2>NEW YORK TIMES</h2>
+              <i class="fas fa-star"></i>
+              <div class="line"></div>
+              <p>
+                Pellentesque vitae viverra risus, sagittis. Venenatis ridiculus
+                scelerisque nisi in urna nulla. Sit tempor a et nisl, ac felis.
+              </p>
+              <p>Pellentesque vitae viverra risus, sagittis.</p>
+              <button class="black-button">READ FULL ARTICLE</button>
+            </div>
+          </div>
+        </div>
+        <div class="big-sotto" style="padding-top: 2rem">
+          <div class="reviews-text-sinistro" style="margin-right: 2rem">
+            <h3>MARY MAXEY</h3>
+            <h2>THE GUARDIAN</h2>
+            <i class="fas fa-star"></i>
+            <div class="line"></div>
+            <p>
+              Pellentesque vitae viverra risus, sagittis. Venenatis ridiculus
+              scelerisque nisi in urna nulla. Sit tempor a et nisl, ac felis.
+            </p>
+            <button class="transp-button">READ FULL ARTICLE</button>
+          </div>
+          <div class="reviews-text-destro">
+            <h3>PATRICK MONROE</h3>
+            <h2>GLOBE AND MAIL</h2>
+            <i class="fas fa-star"></i>
+            <div class="line"></div>
+            <p>
+              Pellentesque vitae viverra risus, sagittis. Venenatis ridiculus
+              scelerisque nisi in urna nulla. Sit tempor a et nisl, ac felis.
+            </p>
+            <button class="transp-button">READ FULL ARTICLE</button>
+          </div>
+        </div>
+      </div>
+
+      <div class="piatto">
+        <p>KUNG PAO CHICKEN - $32</p>
+      </div>
+    </section>
     <!-- sezione 4 -->
     <section></section>
     <!-- sezione 5 -->
@@ -104,6 +156,7 @@ export default {};
   background-image: url(../assets/images/info22x.jpg);
   background-size: cover;
 }
+
 .sushi {
   margin: auto;
   width: 23%;
@@ -111,5 +164,137 @@ export default {};
   flex-direction: column;
   align-items: center;
   padding: 2rem;
+}
+// section 3
+.title-section-3 {
+  text-align: center;
+  padding-bottom: 2rem;
+  padding-top: 2rem;
+}
+.reviews {
+  width: 100%;
+
+  .big {
+    display: flex;
+    width: 75%;
+    margin: auto;
+    .left-box {
+      display: flex;
+      width: 50%;
+      height: 550px;
+      background-image: url(../assets/images/blog72x.jpg);
+      background-size: cover;
+    }
+    .box-sinistra {
+      background-color: $first-color;
+      width: 50%;
+    }
+    .reviews-text {
+      margin: auto;
+      display: flex;
+      flex-direction: column;
+      width: 80%;
+      background-color: $first-color;
+      height: 550px;
+
+      h3 {
+        color: $third-color;
+        font-size: 16px;
+        padding-top: 30px;
+      }
+      h2 {
+        color: $second-color;
+        font-size: 50px;
+        padding-top: 30px;
+      }
+      p {
+        color: $third-color;
+        padding-top: 30px;
+      }
+      button {
+        width: 35%;
+        background-color: $first-color;
+        color: white;
+        border: 1px solid white;
+        padding: 1rem;
+      }
+    }
+  }
+  .big-sotto {
+    display: flex;
+    width: 75%;
+    margin: auto;
+    .reviews-text-sinistro {
+      margin: auto;
+      display: flex;
+      flex-direction: column;
+      width: 80%;
+      background-color: $first-color;
+      height: 500px;
+
+      h3 {
+        color: $third-color;
+        font-size: 16px;
+        padding-top: 30px;
+      }
+      h2 {
+        color: $second-color;
+        font-size: 50px;
+        padding-top: 30px;
+      }
+      p {
+        color: $third-color;
+        padding-top: 30px;
+      }
+      button {
+        width: 35%;
+        background-color: $first-color;
+        color: white;
+        border: 1px solid white;
+        padding: 1rem;
+      }
+    }
+    .reviews-text-destro {
+      margin: auto;
+      display: flex;
+      flex-direction: column;
+      width: 80%;
+      background-color: $first-color;
+      height: 500px;
+
+      h3 {
+        color: $third-color;
+        font-size: 16px;
+        padding-top: 30px;
+      }
+      h2 {
+        color: $second-color;
+        font-size: 50px;
+        padding-top: 30px;
+      }
+      p {
+        color: $third-color;
+        padding-top: 30px;
+      }
+      button {
+        width: 35%;
+        background-color: $first-color;
+        color: white;
+        border: 1px solid white;
+        padding: 1rem;
+      }
+    }
+  }
+}
+.piatto {
+  width: 100%;
+  margin-top: 2rem;
+  background-image: url("../assets/images/page52x.jpg");
+  background-size: cover;
+  display: flex;
+  height: 500px;
+  justify-content: center;
+  align-items: flex-end;
+  color: white;
 }
 </style>
